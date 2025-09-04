b51006b5e5558c3ccd71229f8d89b73723f118b7
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-09-04 21:45:05 +0300

    Initial Commit

---
+++ Healtho Gym/Healtho_GymApp.swift
@@ -0,0 +1,17 @@
+//
+//  Healtho_GymApp.swift
+//  Healtho Gym
+//
+//  Created by Vlad on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Healtho_GymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
